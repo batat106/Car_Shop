cd586d4d5ec14b766da932b5b309ffcf3d77b1cc
Bulat Tkebuchava <93533750+batat106@users.noreply.github.com>
2025-03-28 00:45:43 +0300

    initial commit

---
+++ src/Containers/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className="search">
+            <input type="text" placeholder="Search or type" className="search-box"/>
+            <img src="../images/profile_icon.png" className="img-profile"/>
+        </div>
+    )
+}
+
+export default Header;
